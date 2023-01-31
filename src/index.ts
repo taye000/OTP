c9653ade3e4e7be67ebc632a3368b245729e8dfc
taye000 <taylorgitari@gmail.com>
2023-01-31 17:04:55 +0300

    whatsapp

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 let http = require("http");
 import {Request, Response} from 'express'
+import { whatsapp } from './twilio';
 
 http.createServer(function(req: Request, res:Response){
     res.writeHead(200, {'Content-Type': 'text/plain'})
     res.end("mofo")
 }).listen(8081);
+
+whatsapp()
 
 console.log("listening at localhost 8081");
--- src/twilio/twilio.ts
+++ src/twilio/twilio.ts
@@ -1,14 +1,24 @@
-import { Twilio } from "twilio";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const accountSid = process.env.ACCOUNT_SID!;
 const authToken = process.env.AUTH_TOKEN!;
 
-const client = new Twilio(accountSid, authToken);
+const client = require("twilio")(accountSid, authToken, {
+  lazyLoading: false,
+});
 
-export const whatsapp = async (OTP: any) => {
-  await client.messages.create({
-    from: "whatsapp:+254712069869",
-    to: "whatsapp:+254710518",
-    body: "Buuda",
-  });
+export const whatsapp = () => {
+  client.messages
+    .create({
+      body: "Go F yourself",
+      to: "whatsapp:+254712069869",
+      from: "whatsapp:+14155238886",
+    })
+    .then((message: { sid: any }) =>
+      console.log(
+        `Message succesfully sent, Here is the message SID ${message.sid}`
+      )
+    )
+    .done();
 };
